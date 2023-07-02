add820b3d1ea34f8abd05b56660b20339d2d7245
iruzo <dev@iruzo.dev>
2023-07-02 22:36:35 +0200

    [WIP] one-cell config file

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,36 +1,56 @@
 pub mod sync;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use std::sync::{Arc, Mutex};
 use std::thread;
 use crate::path;
+use once_cell::sync::Lazy;
 
-fn read_lines(file_path: String) -> Vec<std::string::String> {
+static BLACKLIST: Lazy<Mutex<Vec<String>>> = Lazy::new(|| {
+    let bl = Mutex::new(read_lines(path::blacklist_file()));
+    bl
+});
+
+
+static WHITELIST: Lazy<Arc<Mutex<Vec<String>>>> = Lazy::new(|| {
+    let wl = Arc::new(Mutex::new(read_lines(path::whitelist_file())));
+    wl
+});
+
+fn read_lines(file_path: String) -> Vec<String> {
 
     let file = File::open(file_path).unwrap();
-    let reader = BufReader::new(file);
-    let mut lines = Vec::new();
-
-    for line in reader.lines() {
-        if let Ok(line) = line {
-            lines.push(line)
-        }
-    }
+    let lines: Vec<String> = BufReader::new(file).lines().filter_map(|l| l.ok()).collect();
 
     return lines;
 
 }
 
-pub fn blacklist() -> Vec<String> {
+pub fn blacklist(refresh: bool) -> Mutex<Vec<String>> {
 
-    let ips: Vec<String> = read_lines(path::blacklist_file());
-    return ips;
+    if refresh {
+        let mut lines: Vec<String> = read_lines(path::blacklist_file());
+        let mut lock = BLACKLIST.lock().unwrap();
+        lock.clear();
+        lock.append(&mut lines);
+    }
+
+    return BLACKLIST.lock().unwrap();
 
 }
 
-pub fn whitelist() -> Vec<String> {
+pub fn whitelist(refresh: bool) -> Arc<Mutex<Vec<String>>> {
 
-    let ips: Vec<String> = read_lines(path::whitelist_file());
-    return ips;
+    if refresh {
+        let lines: Vec<String> = read_lines(path::blacklist_file());
+        // check how to change vector capacity
+        WHITELIST.clone().lock().unwrap().clear();
+        for line in lines {
+            WHITELIST.clone().lock().unwrap().push(line);
+        }
+    }
+
+    return WHITELIST.clone();
 
 }
 
